23edb1b857657597e70f3ee851cc45f759eeaee7
almas-josephine <almas.josephine.placides@gmail.com>
2025-06-21 10:28:19 -0400

    first commit

---
+++ app/sessions/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+    return (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css" />
+            <div className="w3-container w3-center">
+                <div className=" m1 w3-center">
+                    <h1>Sessions</h1>
+                </div>
+            </div>
+        </>
+    );
+}
