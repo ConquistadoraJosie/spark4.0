1a6d10d19448ca50940be09f5ff9c42b20dcfba8
almas-josephine <almas.josephine.placides@gmail.com>
2025-06-21 18:23:19 -0400

    last update on frontend

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,9 @@ export default function Home() {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css" />
-      <div className="responsive-two-column-grid">
-        <div>
-          <Image src={mission} alt="mission statement" />
-        </div>
-        <div>Image carosel TBP</div>
+      <div className="row">
+        <div className="column"><Image src={mission} alt="mission statement" width={700} /></div>
+        <div className="column">TBP</div>
       </div>
     </>
   );
